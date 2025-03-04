b6a1c5c67c18548a04b977facb7246ac6d62091c
averieyy <58344917+averieyy@users.noreply.github.com>
2025-03-04 19:26:29 +0100

    Fixed and styled header links

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
+  import { page } from "$app/state";
+
   const { client }: { client: undefined | object } = $props();
 </script>
 
 <header>
-  <h1>Speedrun</h1>
+  <a class="rootlink" href="/">
+    <h1>Speedrun</h1>
+  </a>
   <div class="client">
     {#if client}
       <a href="/user" class="button">My page</a>
     {:else}
-      <a href="/login" class="button">Login</a>
-      <a href="/register" class="button">Register</a>
+      <a href="/login?redirect={page.url.pathname}" class="button">Login</a>
+      <a href="/register?redirect={page.url.pathname}" class="button">Register</a>
     {/if}
   </div>
 </header>
@@ -30,5 +34,24 @@
     display: flex;
     flex-direction: row;
     gap: .5rem;
+  }
+  h1 {
+    border-image: linear-gradient(to left, var(--emphasis), var(--emphasis-dark)) 1;
+    border-bottom: .125rem solid;
+    transition: border-bottom-width .125s ease;
+  }
+  .rootlink {
+    text-decoration: none;
+    height: 100%;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    
+    &:hover, &:active {
+      &>h1 {
+        border-bottom-width: .25rem;
+      }
+    }
   }
 </style>
