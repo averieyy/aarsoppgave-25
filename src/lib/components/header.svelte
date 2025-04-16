0f70a99025e0c5c506a86b6c43561cee81e7edfa
averieyy <58344917+averieyy@users.noreply.github.com>
2025-04-16 13:28:20 +0200

    Added navigation to the header

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -6,9 +6,14 @@
 </script>
 
 <header>
-  <a class="rootlink" href="/">
-    <h1>Speedrun</h1>
-  </a>
+  <nav>
+    <a class="rootlink" href="/">
+      <h1>Speedrun</h1>
+    </a>
+    <span class="sep smallhidden"></span>
+    <a class="smallhidden navbutton" href="/about">About</a>
+    <a class="smallhidden navbutton" href="/games">Games</a>
+  </nav>
   <div class="client">
     {#if client}
       <a href="/user" class="button {client.profile_pic ? 'withprofilepic' : 'withoutprofilepic'}">
@@ -26,6 +31,12 @@
 </header>
 
 <style>
+  @media screen and (max-width: 500px) {
+    .smallhidden {
+      display: none !important;
+    }
+  }
+
   .withprofilepic {
     height: 3rem;
     width: 3rem;
@@ -67,11 +78,39 @@
     display: flex;
     flex-direction: row;
     align-items: center;
-    
+    margin: .5rem;
+
     &:hover, &:active {
-      &>h1 {
+      & h1 {
         border-bottom-width: .25rem;
       }
+    }
+  }
+  nav {
+    display: flex;
+    flex-direction: row;
+    gap: .5rem;
+    align-items: center;
+  }
+  .sep {
+    width: .125rem;
+    margin: .5rem;
+    height: 1.5rem;
+    background-color: var(--emphasis);
+  }
+  .navbutton {
+    text-decoration: none;
+    padding: .5rem;
+    box-sizing: border-box;
+    height: 2.5rem;
+
+    display: flex;
+    align-items: center;
+
+    background-color: var(--bg2);
+    
+    &:hover, &:active, &:focus {
+      background-color: var(--bg3);
     }
   }
 </style>
