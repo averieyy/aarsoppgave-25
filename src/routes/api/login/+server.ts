506d155627501b2c34c3a0fbb3fb78f4eb273d03
averieyy <58344917+averieyy@users.noreply.github.com>
2025-04-22 08:59:15 +0200

    Made it impossible to log into deleted users

--- src/routes/api/login/+server.ts
+++ src/routes/api/login/+server.ts
@@ -17,13 +17,13 @@ export const POST: RequestHandler = async ({ cookies, request }) => {
 
   if (!username || !password) return json({ message: 'Username or password missing' }, { status: 400 });
 
-  const clientstruct = await db.queryOne<client>('select * from clients where username = $1::text', username);
-  if (!clientstruct) return json({ message: 'User not found' }, { status: 400 });
+  const existingclient = await db.queryOne<client>('select * from clients where username = $1::text and deleted = false', username);
+  if (!existingclient) return json({ message: 'User not found' }, { status: 400 });
 
-  if (Client.hashPassword(password, clientstruct.salt) != clientstruct.hash)
+  if (Client.hashPassword(password, existingclient.salt) != existingclient.hash)
     return json({ message: 'Wrong username or password' }, { status: 400 });
 
-  const client = Client.fromStruct(clientstruct);
+  const client = Client.fromStruct(existingclient);
 
   await client.newToken(cookies);
 
